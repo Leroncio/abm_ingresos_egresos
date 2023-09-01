d55dd2779fc100edc0e07741894691cc979b8a35
Eduardo Araya <arayacortes.edu@gmail.com>
2023-09-01 19:56:26 -0300

    preparacion de estructura db mediante seeders y migrations

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login',  [LoginController::class, 'returnLogin'])->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
